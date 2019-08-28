5694d13bb4f3dc748f24707241ff6f90656dc07b
Jeff Au <jeffau13@gmail.com>
2019-08-28 13:26:48 -0400

    defined a reference schema

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+// Reference to db schema
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2019-08-28T17:24:40.011Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
