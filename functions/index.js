01be7cd5b4a4fe24f78b5472410ee3f04c1177fd
Jeff Au <jeffau13@gmail.com>
2019-08-28 14:27:51 -0400

    signup route with firebase auth

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
-
+const firebase = require('firebase');
 const app = express();
 admin.initializeApp();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFqd2Qjs4Csf6CMc6wKONa0MNAv88DKn4',
+  authDomain: 'socialape-8fb19.firebaseapp.com',
+  databaseURL: 'https://socialape-8fb19.firebaseio.com',
+  projectId: 'socialape-8fb19',
+  storageBucket: 'socialape-8fb19.appspot.com',
+  messagingSenderId: '420707583038',
+  appId: '1:420707583038:web:ba903df403ec9f96'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 app.get('/screams', (req, res) => {
   admin
@@ -26,8 +38,8 @@ app.get('/screams', (req, res) => {
 app.post('/screams', (req, res) => {
   const { body, userHandle } = req.body;
   const newScream = {
-    body: body,
-    userHandle: userHandle,
+    body,
+    userHandle,
     createdAt: new Date().toISOString()
   };
 
@@ -41,6 +53,32 @@ app.post('/screams', (req, res) => {
     .catch(err => {
       response.status(500).json({ error: `Something went wrong` });
       console.error(err);
+    });
+});
+
+// signup route
+app.post('/signup', (req, res) => {
+  const { email, password, confirmPassword, handle } = req.body;
+  const newUser = {
+    email,
+    password,
+    confirmPassword,
+    handle
+  };
+
+  // TODO: validate data
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
 });
 
