b68bf2e6664c40d444f718ff1016c89270b43c40
Jeff Au <jeffau13@gmail.com>
2019-09-04 12:46:40 -0400

    refactored

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: 'socialape-8fb19.firebaseapp.com',
+  databaseURL: 'https://socialape-8fb19.firebaseio.com',
+  projectId: 'socialape-8fb19',
+  storageBucket: 'socialape-8fb19.appspot.com',
+  messagingSenderId: '420707583038',
+  appId: '1:420707583038:web:ba903df403ec9f96'
+};
