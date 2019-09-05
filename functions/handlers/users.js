dd27d3bf3b79fb945471fcd830fc1f7c3f081097
Jeff Au <jeffau13@gmail.com>
2019-09-05 18:46:52 -0400

    created handler for image upload

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -89,3 +89,27 @@ exports.login = (req, res) => {
       }
     });
 };
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    // index of last item in split array
+    console.table(fieldname, filename, mimetype);
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 100000000
+    )}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+};
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const functions = require('firebase-functions');
 const express = require('express');
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { signup, login, uploadImage } = require('./handlers/users');
 const FBAuth = require('./util/FBAuth');
 
 const app = express();
@@ -14,6 +14,7 @@ app.post('/screams', FBAuth, postOneScream);
 // user routes
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', uploadImage);
 
 // https://example.com/api/...:
 exports.api = functions.https.onRequest(app);
