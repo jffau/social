581bf79d70d2223555b1cafff4863c5a41a95431
Jeff Au <jeffau13@gmail.com>
2019-09-05 21:07:18 -0400

    updated schema

--- dbschema.js
+++ dbschema.js
@@ -22,3 +22,27 @@ let db = {
     }
   ]
 };
+
+let userDetails = {
+  // front end state data
+  credentials: {
+    userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, I am a user',
+    website: 'https://user.com',
+    location: 'Lonodn, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa'
+    },
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO'
+    }
+  ]
+};
