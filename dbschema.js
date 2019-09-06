3e82d60651b44bed461625d0940c00f55c844a81
Jeff Au <jeffau13@gmail.com>
2019-09-05 20:50:52 -0400

    updated schema

--- dbschema.js
+++ dbschema.js
@@ -1,5 +1,17 @@
 // Reference to db schema
 let db = {
+  users: [
+    {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, I am a user',
+      website: 'https://user.com',
+      location: 'Lonodn, UK'
+    }
+  ],
   screams: [
     {
       userHandle: 'user',
