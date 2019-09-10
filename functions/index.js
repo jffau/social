aeca7df3a49381d69cf7976cbe17c36d44daac09
Jeff Au <jeffau13@gmail.com>
2019-09-09 21:19:55 -0400

    notification functions  for comment and unlike

--- functions/index.js
+++ functions/index.js
@@ -45,7 +45,7 @@ exports.api = functions.https.onRequest(app);
 
 // database trigger
 exports.createNotificationOnLike = functions
-  .region('europe-west1')
+  .region('us-central1')
   .firestore.document('likes/{id}')
   .onCreate(snapshot => {
     return db
@@ -67,4 +67,45 @@ exports.createNotificationOnLike = functions
         }
       })
       .catch(err => console.error(err));
+  });
+
+exports.deleteNotificationOnUnLike = functions
+  .region('us-central1')
+  .firestore.document('likes/{id}')
+  .onDelete(snapshot => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region('us-central1')
+  .firestore.document('comments/{id}')
+  .onCreate(snapshot => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
   });
