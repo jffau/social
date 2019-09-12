a13464ac21586dbf1a521ab458d907de9a36c547
Jeff Au <jeffau13@gmail.com>
2019-09-11 21:23:46 -0400

    Router and three main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       My app
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+    render() {
+        return (
+            <h1>Login Page</h1>
+        )
+    }
+}
+
+export default Login
