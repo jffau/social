1906c9b64ac1e9ae3b87d39036a2680e67855b3d
Jeff Au <jeffau13@gmail.com>
2019-09-12 14:55:06 -0400

    created color pallette in theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+// Mui
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 // pages:
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
-
 // components:
 import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#7c00d4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div className="container">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
           <Navbar />
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
