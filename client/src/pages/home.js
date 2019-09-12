ce273c5e542aa3b08061295447a42833ea85ed14
Jeff Au <jeffau13@gmail.com>
2019-09-12 15:56:09 -0400

    getting posts from db

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 export class Home extends Component {
+  state = {
+    posts: null
+  };
+
+  componentDidMount() {
+    axios
+      .get('/screams')
+      .then(res => {
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render() {
-    return <h1>Home Page</h1>;
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <p> {post.body}</p>)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={8}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
