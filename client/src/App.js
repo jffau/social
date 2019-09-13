f853091e8c601150b2587db14b41890dc87fdb63
Jeff Au <jeffau13@gmail.com>
2019-09-12 21:22:13 -0400

    storing jwt to local storage. AuthRoute redirtects if logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import './App.css';
 import themeOptions from './util/theme';
 // Mui
@@ -12,8 +13,23 @@ import login from './pages/login';
 import signup from './pages/signup';
 // components:
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 const theme = createMuiTheme(themeOptions);
+
+const token = localStorage.FBIdToken;
+let authenticated;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  // Compare against now *1000 because it is in seconds.
+  if (decodedToken.exp * 1000 < Date.now()) {
+    authenticated = false;
+    window.location.href = '/login';
+  } else {
+    authenticated = true;
+  }
+}
+
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
@@ -23,8 +39,18 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
