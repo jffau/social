09c16d40a029f6295cc839144f3e0954936e6e88
Jeff Au <jeffau13@gmail.com>
2019-09-16 21:27:58 -0400

    created Profile section

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import Post from '../components/Post';
+import Profile from '../components/Profile';
+
 export class Home extends Component {
   state = {
     posts: null
@@ -32,7 +34,7 @@ export class Home extends Component {
           {recentPostsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>Profile</p>
+          <Profile />
         </Grid>
       </Grid>
     );
