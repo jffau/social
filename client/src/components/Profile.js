64829fdc32f6e30fc2989080475369fbb6b39869
Jeff Au <jeffau13@gmail.com>
2019-09-30 16:50:42 -0400

    created dialog UI for editing user details

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -147,6 +148,7 @@ class Profile extends Component {
                 <KeyboardReturn color="primary" />
               </IconButton>
             </Tooltip>
+            <EditDetails />
           </div>
         </Paper>
       ) : (
